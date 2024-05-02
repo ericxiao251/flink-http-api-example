a6093507aaee13efd563bfd307121b5251015627
Eric Xiao <9758510+ericxiao251@users.noreply.github.com>
2024-05-02 10:30:22 -0400

    Work with one pokemon.

--- flink-http-api-example/src/main/java/org/example/Main.java
+++ flink-http-api-example/src/main/java/org/example/Main.java
@@ -1,15 +1,12 @@
 package org.example;
 
-import org.apache.flink.api.java.tuple.Tuple2;
+
 import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.async.AsyncRetryStrategy;
-import org.apache.flink.streaming.util.retryable.AsyncRetryStrategies;
-import org.apache.flink.streaming.util.retryable.RetryPredicates;
 
-import javax.xml.crypto.Data;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -17,11 +14,11 @@ public class Main {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<String> inputStream = env.fromElements(
-                "pokemon/5",
-                "pokemon/6",
-                "pokemon/7",
-                "pokemon/8",
-                "pokemon/9"
+                "pokemon/5"
+//                ,"pokemon/6"
+//                ,"pokemon/7"
+//                ,"pokemon/8"
+//                ,"pokemon/9"
         );
 
         DataStream<String> outputStream = AsyncDataStream.unorderedWait(
