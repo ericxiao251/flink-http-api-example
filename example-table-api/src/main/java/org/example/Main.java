d6092a306a3bc3368cf7a27277e81114f52c2fe2
Eric Xiao <9758510+ericxiao251@users.noreply.github.com>
2024-05-23 12:44:29 -0400

    Example of using the table API.

--- example-table-api/src/main/java/org/example/Main.java
+++ example-table-api/src/main/java/org/example/Main.java
@@ -4,23 +4,35 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.types.Row;
 
+// imports for Table API with bridging to Java DataStream API
+import org.apache.flink.table.api.*;
+import org.apache.flink.table.api.bridge.java.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         // https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/dev/table/data_stream_api/
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
         DataStream<Row> leftStream = env.fromElements(
                 Row.of(1, "a"),
                 Row.of(2, "b"),
                 Row.of(3, "c"));
-        leftStream.print();
+        Table leftTable = tableEnv.fromDataStream(leftStream).as("id", "name");
+        tableEnv.createTemporaryView("leftTable", leftTable);
 
         DataStream<Row> rightStream = env.fromElements(
-                Row.of(1, "a"),
-                Row.of(2, "b"),
-                Row.of(2, "c")
+                Row.of(1, "1"),
+                Row.of(2, "1"),
+                Row.of(2, "2")
         );
-        rightStream.print();
+        Table rightTable = tableEnv.fromDataStream(rightStream).as("id", "version");
+        tableEnv.createTemporaryView("rightTable", rightTable);
+
+        Table resultTable = tableEnv.sqlQuery(
+                "SELECT l.*, r.version FROM leftTable AS l INNER JOIN rightTable AS r ON l.id = r.id");
+        DataStream<Row> resultStream = tableEnv.toChangelogStream(resultTable);
+        resultStream.print();
 
         env.execute();
     }
